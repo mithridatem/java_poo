a8bdde0deacc5f7beae008b8284231e663963b31
mithridatem <mathieumithridate@adrar-formation.com>
2024-03-20 09:34:01 +0100

    initial commit

---
+++ src/com/cda2/classe/Utilisateur.java
@@ -0,0 +1,59 @@
+package com.cda2.classe;
+
+public class Utilisateur {
+    //attributs
+    private String nom;
+    private String prenom;
+    private String email;
+    private String password;
+
+    //constructeurs
+    public Utilisateur(){};
+
+    public Utilisateur(String nom, String prenom, String email, String password) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.password = password;
+    }
+
+    //getter et setters
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //méthode
+    public String toString() {
+        return this.nom + " " + this.prenom;
+    }
+}
